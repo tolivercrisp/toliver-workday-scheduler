dc5d6e9d54d9b230eada7ca94bf858f7532ad1e7
Toliver Crisp <toliverjcrisp@gmail.com>
2022-10-18 23:56:50 -0400

    Accurately target the textarea ID to compare it to the currentTime in hours

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,26 @@
+// Dynamic Date Display in the header
+var today = moment();
+console.log(today)
+$("#currentDay").text(today.format("dddd, MMM Do"));
+
 // Stores the current hour (as an integer) into a variable
 var currentHour = 12;
-
 
 // moment().hour();
 console.log(currentHour)
 
 
-// Date Display
-var today = moment();
-console.log(today)
-$("#currentDay").text(today.format("dddd, MMM Do"));
-
-
-
-
-
-// changes <textarea> color based on the time of day
+// function that changes <textarea> color based on the time of day
 function changeHourColor () {
   $(".time-block").each(function () {
     var plannerHour = $(this).attr("id");
     if (plannerHour < currentHour) {
       $(this).children("textarea").addClass("past");
+    } else if (plannerHour == currentHour) {
+      $(this).children("textarea").addClass("present");
+    } else {
+      $(this).children("textarea").addClass("future");
+
     }
 
   })
