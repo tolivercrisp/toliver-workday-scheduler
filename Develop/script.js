56953d7b13fb182a61e65f40be6fb4dfd9a59816
Toliver Crisp <toliverjcrisp@gmail.com>
2022-10-18 23:53:01 -0400

    Switched stale js array and variable list to a more optimized jQuery layout

--- Develop/script.js
+++ Develop/script.js
@@ -12,59 +12,18 @@ console.log(today)
 $("#currentDay").text(today.format("dddd, MMM Do"));
 
 
-// Assigns HTML element to a JS variable
-var hourNine = $('#9am');
-var hourTen = $('#10am');
-var hourEleven = $('#11am');
-var hourTwelve = $('#12pm');
-var hourOne = $('#1pm');
-var hourTwo = $('#2pm');
-var hourThree = $('#3pm');
-var hourFour = $('#4pm');
-var hourFive = $('#5pm');
 
-// Assigns the new JS variables a numeric value
-
-hourNine = 9;
-hourTen = 10;
-hourEleven = 11;
-hourTwelve = 12;
-hourOne = 13;
-hourTwo = 14;
-hourThree = 15;
-hourFour = 16;
-hourFive = 17;
-
-
-
-// Places variables into an array
-var hourArray = [
-    hourNine,
-    hourTen,
-    hourEleven,
-    hourTwelve,
-    hourOne,
-    hourTwo,
-    hourThree,
-    hourFour,
-    hourFive
-];
-console.log(hourArray)
-var hourArrayNew = hourArray.map(changeHourColor);
 
 
 // changes <textarea> color based on the time of day
-function changeHourColor (hourArrayNew) {
-  for (let i = 0; i < hourArray.length; i++) {
-  if (currentHour === hourArray.splice()) {
-    $( "textarea" ).addClass('present');
-  } else if (currentHour < hourArray.splice()) {
-    $( "textarea" ).addClass('past');
-  } else {
-    $( "textarea" ).addClass('future');
-  }
-    
-  }
+function changeHourColor () {
+  $(".time-block").each(function () {
+    var plannerHour = $(this).attr("id");
+    if (plannerHour < currentHour) {
+      $(this).children("textarea").addClass("past");
+    }
+
+  })
   
 }
   
