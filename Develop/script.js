aba141cdc3a60eb24af0a3cf1cbd08ef8a164051
Toliver Crisp <toliverjcrisp@gmail.com>
2022-10-18 23:48:48 -0400

    Restructured HTML to put the ID tags in the parent <div>s instead of <textarea>

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,18 @@
+// Stores the current hour (as an integer) into a variable
+var currentHour = 12;
+
+
+// moment().hour();
+console.log(currentHour)
+
+
+// Date Display
+var today = moment();
+console.log(today)
+$("#currentDay").text(today.format("dddd, MMM Do"));
+
+
+// Assigns HTML element to a JS variable
 var hourNine = $('#9am');
 var hourTen = $('#10am');
 var hourEleven = $('#11am');
@@ -8,6 +23,21 @@ var hourThree = $('#3pm');
 var hourFour = $('#4pm');
 var hourFive = $('#5pm');
 
+// Assigns the new JS variables a numeric value
+
+hourNine = 9;
+hourTen = 10;
+hourEleven = 11;
+hourTwelve = 12;
+hourOne = 13;
+hourTwo = 14;
+hourThree = 15;
+hourFour = 16;
+hourFive = 17;
+
+
+
+// Places variables into an array
 var hourArray = [
     hourNine,
     hourTen,
@@ -20,25 +50,29 @@ var hourArray = [
     hourFive
 ];
 console.log(hourArray)
-
-
-var currentHour = moment().hour();
-console.log(currentHour)
-
-
-// Date Display
-var today = moment();
-console.log(today)
-$("#currentDay").text(today.format("dddd, MMM Do"));
+var hourArrayNew = hourArray.map(changeHourColor);
 
 
 // changes <textarea> color based on the time of day
-$(function () {
-
-
-  });
-
-
-  if (currenthour === 12) {
-    make this ID 
+function changeHourColor (hourArrayNew) {
+  for (let i = 0; i < hourArray.length; i++) {
+  if (currentHour === hourArray.splice()) {
+    $( "textarea" ).addClass('present');
+  } else if (currentHour < hourArray.splice()) {
+    $( "textarea" ).addClass('past');
+  } else {
+    $( "textarea" ).addClass('future');
   }
+    
+  }
+  
+}
+  
+changeHourColor();
+
+ 
+
+// function --> when button is clicked, save textarea value to local storage
+
+// $( "#button-container button" ).on( "click", function( event ) {
+// });
